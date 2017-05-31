0a0a07146d896fb5b5f938feabe30df0584ca5e5
luhaiming <luhaiming@zillionfortune.com>
2017-05-31 09:55:22 +0800

    first commit for

---
+++ bi/src/test/java/com/zillionfortune/bi/login/Login.java
@@ -0,0 +1,14 @@
+package com.zillionfortune.bi.login;
+
+import org.junit.Test;
+
+/**
+ * Created by luhaiming on 2017/1/6 0006.
+ */
+public class Login {
+
+    @Test
+    public void testLogin(){
+
+    }
+}
---
+++ seleniumext/src/main/java/com.zillionfortune/seleniumext/WaitConditions.java
@@ -0,0 +1,7 @@
+package com.zillionfortune.seleniumext;
+
+/**
+ * Created by luhaiming on 2017/1/4 0004.
+ */
+public class WaitConditions {
+}
